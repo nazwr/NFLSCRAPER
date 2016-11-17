d64b43a9def403390baa1f02ed9bc96d48063c31
Nathan W <nate01776@gmail.com>
2016-11-17 14:32:09 -0500

    rewrite migration

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,3 @@
 class Player < ActiveRecord::Base
+  validates :name, presence: true
 end
--- db/migrate/20161117191647_add_players.rb
+++ db/migrate/20161117191647_add_players.rb
@@ -1,7 +1,8 @@
 class AddPlayers < ActiveRecord::Migration[5.0]
   def change
     create_table :players do |t|
-      t.string :name, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.timestamps
     end
   end
