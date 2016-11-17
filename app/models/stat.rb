eb383cd6bd1fe152f82970240ab82ed3f0ba7c31
Nathan W <nate01776@gmail.com>
2016-11-17 18:55:50 -0500

    added plain text stat method

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -3,4 +3,8 @@ class Stat < ActiveRecord::Base
   validates :player, presence: true
 
   belongs_to :player
+
+  def plain_text
+    "#{player.full_name} #{play_type}es to the #{direction} for #{yards} yards"
+  end
 end
