b820cada3158a73fb297bc664e23339c227d53be
Jun Oh <jsohpennstater@gmail.com>
2016-11-25 15:35:50 -0500

    commented out scrapers

--- public/player_scraper.rb
+++ public/player_scraper.rb
@@ -1,30 +1,30 @@
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
-require 'json'
+# require 'nokogiri'
+# require 'open-uri'
+# require 'pry'
+# require 'json'
 # PLAYER INFOMATION FROM ROSTER
-response = Nokogiri::HTML(open('http://www.nfl.com/teams/baltimoreravens/roster?team=BAL'))
-
-rows = response.css('tr')
-data = []
-
-rows.each do |element|
-  if element.children[3] == nil
-    puts 'skipped ///////////////'
-  elsif element.children[3].children.last.attributes.first.last.value[1..6] == "player"
-    new_player = {}
-    new_player[:name] = element.children[3].children.last.children.text
-    new_player[:position] = element.children[5].children.text
-    new_player[:status] = element.children[7].children.text
-    new_player[:height] = element.children[9].children.text
-    new_player[:weight] = element.children[11].children.text
-    new_player[:birthdate] = element.children[13].children.text
-    new_player[:years_pro] = element.children[15].children.text
-    new_player[:college] = element.children[17].children.text
-    new_player[:profile_URL] = element.children[3].children.last.attributes["href"].value
-
-    data << new_player
-  end
-end
-
-binding.pry
+# response = Nokogiri::HTML(open('http://www.nfl.com/teams/baltimoreravens/roster?team=BAL'))
+#
+# rows = response.css('tr')
+# data = []
+#
+# rows.each do |element|
+#   if element.children[3] == nil
+#     puts 'skipped ///////////////'
+#   elsif element.children[3].children.last.attributes.first.last.value[1..6] == "player"
+#     new_player = {}
+#     new_player[:name] = element.children[3].children.last.children.text
+#     new_player[:position] = element.children[5].children.text
+#     new_player[:status] = element.children[7].children.text
+#     new_player[:height] = element.children[9].children.text
+#     new_player[:weight] = element.children[11].children.text
+#     new_player[:birthdate] = element.children[13].children.text
+#     new_player[:years_pro] = element.children[15].children.text
+#     new_player[:college] = element.children[17].children.text
+#     new_player[:profile_URL] = element.children[3].children.last.attributes["href"].value
+#
+#     data << new_player
+#   end
+# end
+#
+# binding.pry
--- public/stat_scraper.rb
+++ public/stat_scraper.rb
@@ -1,51 +1,51 @@
-require 'open-uri'
-require 'nokogiri'
-require 'pry'
-require 'rubygems'
-require 'json'
+# require 'open-uri'
+# require 'nokogiri'
+# require 'pry'
+# require 'rubygems'
+# require 'json'
 
 # game_week = 2
 # while game_week <= 11
-  doc = Nokogiri::HTML(open("http://scores.nbcsports.com/fb/scoreboard.asp?week=#{game_week}"))
-  a = doc.css('a')
-  array = []
-  a.each do |thing|
-    if thing.text == "Box"
-      array << thing
-    end
-  end
-
-
-  gamecodes = []
-  array.each do |href|
-    b = href.attributes["href"].value.split("=")[1].split("&").first
-    gamecodes << b
-  end
-
-
-  stat = Hash.new
-  gamecodes.each do |gamecode|
-    gamestatarray = []
-    gameday_uri = "http://scores.nbcsports.com/fb/pbp.asp?gamecode=#{gamecode}"
-    gamestat = Nokogiri::HTML(open(gameday_uri))
-    c = gamestat.css('tr')
-
-    c.each do |gamestat|
-      begin
-        val = gamestat.children.last.children.last.attributes["class"].value
-        if val == "shsPlayDetails"
-          gamestatarray << gamestat.children.last.children.last.text
-        end
-      rescue Exception => e
-        next
-      end
-    end
-    stat[gamecode] = gamestatarray
-    File.open("#{gamecode}.json","w") do |f|
-      f.write(stat[gamecode].to_json)
-    end
-
-  end
+  # doc = Nokogiri::HTML(open("http://scores.nbcsports.com/fb/scoreboard.asp?week=#{game_week}"))
+  # a = doc.css('a')
+  # array = []
+  # a.each do |thing|
+  #   if thing.text == "Box"
+  #     array << thing
+  #   end
+  # end
+  #
+  #
+  # gamecodes = []
+  # array.each do |href|
+  #   b = href.attributes["href"].value.split("=")[1].split("&").first
+  #   gamecodes << b
+  # end
+  #
+  #
+  # stat = Hash.new
+  # gamecodes.each do |gamecode|
+  #   gamestatarray = []
+  #   gameday_uri = "http://scores.nbcsports.com/fb/pbp.asp?gamecode=#{gamecode}"
+  #   gamestat = Nokogiri::HTML(open(gameday_uri))
+  #   c = gamestat.css('tr')
+  #
+  #   c.each do |gamestat|
+  #     begin
+  #       val = gamestat.children.last.children.last.attributes["class"].value
+  #       if val == "shsPlayDetails"
+  #         gamestatarray << gamestat.children.last.children.last.text
+  #       end
+  #     rescue Exception => e
+  #       next
+  #     end
+  #   end
+  #   stat[gamecode] = gamestatarray
+  #   File.open("#{gamecode}.json","w") do |f|
+  #     f.write(stat[gamecode].to_json)
+  #   end
+  #
+  # end
 
 #   game_week += 1
 # end
