e7779753315a6cda61bd258f7aee169788f6a56f
Jun Oh <jsohpennstater@gmail.com>
2016-11-28 12:40:06 -0500

    changed logic for name edge case for lowercase

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -51,8 +51,8 @@ class App extends Component {
   handleNewPlayerSearch() {
     let searchFirstName = this.state.playerSearchFirstName.trim();
     let searchLastName = this.state.playerSearchLastName.trim();
-    let firstName = searchFirstName[0].toUpperCase() + searchFirstName.slice(1).toLowerCase();
-    let lastName = searchLastName[0].toUpperCase() + searchLastName.slice(1).toLowerCase();
+    let firstName = searchFirstName[0].toUpperCase() + searchFirstName.slice(1);
+    let lastName = searchLastName[0].toUpperCase() + searchLastName.slice(1);
     this.setState({});
     $.ajax({
       url: `api/v1/players`,
