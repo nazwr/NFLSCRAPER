6a444eff5f0eb9c9207c6292559d82e8c2905e28
Jun Oh <jsohpennstater@gmail.com>
2016-12-23 13:49:06 -0500

    Game controller grabs player's gametotal

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -3,7 +3,25 @@ class Api::V1::GamesController < ApiController
     @weeks = Game.all.pluck(:week).uniq
     @games = Game.where(week: params["week"])
     @stats = Stat.where(gamecode: params["gamecode"])
-    @players = Player.all
+    @players = @stats.all.pluck(:player_id).uniq
+
+    @playertotal = Hash.new
+    @players.each do |player|
+      a = Player.find_by(id: player)
+      @gametotal = Hash.new
+      @gametotal["passing_yards"] = Stat.new.total_pass_yards(a.first_name,a.last_name,params[:gamecode])
+      @gametotal["passing_tds"] = Stat.new.total_pass_tds(a.first_name,a.last_name,params[:gamecode])
+      @gametotal["interceptions"] = Stat.new.total_interceptions(a.first_name,a.last_name,params[:gamecode])
+      @gametotal["completions"] = Stat.new.completions(a.first_name,a.last_name,params[:gamecode])
+      @gametotal["attempts"] = Stat.new.attempts(a.first_name,a.last_name,params[:gamecode])
+      @gametotal["rushing_attempts"] = Stat.new.rushing_attempts(a.first_name,a.last_name,params[:gamecode])
+      @gametotal["rushing_yards"] = Stat.new.total_rush_yards(a.first_name,a.last_name,params[:gamecode])
+      @gametotal["rushing_tds"] = Stat.new.total_rush_tds(a.first_name,a.last_name,params[:gamecode])
+      @gametotal["receptions"] = Stat.new.receptions(a.first_name,a.last_name,params[:gamecode])
+      @gametotal["receiving_yards"] = Stat.new.total_rec_yards(a.first_name,a.last_name,params[:gamecode])
+      @gametotal["receiving_tds"] = Stat.new.total_rec_tds(a.first_name,a.last_name,params[:gamecode])
+      @playertotal[a.full_name] = @gametotal
+    end
 
     render json: {
     weeks: @weeks,
