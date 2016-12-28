e0204f83a83cf6b6af6c1f5ad873d296407292bc
Jun Oh <jsohpennstater@gmail.com>
2016-12-28 15:11:44 -0500

    passing tests for stat model

--- spec/models/stat_spec.rb
+++ spec/models/stat_spec.rb
@@ -28,55 +28,55 @@ describe "Player's Game Total: " do
   let!(:stat6) { FactoryGirl.create(:stat, play_type: "rush", complete: false, yards: "1", intercepted: false, touchdown: true, player: brady) }
   let!(:stat7) { FactoryGirl.create(:stat, play_type: "rec", complete: false, yards: "25", intercepted: false, touchdown: false, player: brady) }
 
-  it "Brady's total passing tds is equal to 2" do
-    expect(brady.total_pass_tds).to eq(2)
+  it "Brady's game total passing tds is equal to 2" do
+    expect(Stat.new.total_pass_tds(brady.first_name, brady.last_name, game.gamecode)).to eq(2)
   end
 
-  it "Brady's total passing interceptions is equal to 1" do
-    expect(brady.total_interceptions).to eq(1)
+  it "Brady's game total passing interceptions is equal to 1" do
+    expect(Stat.new.total_interceptions(brady.first_name, brady.last_name, game.gamecode)).to eq(1)
   end
 
-  it "Brady's total passing yards is equal to 30" do
-    expect(brady.total_pass_yards).to eq(30)
+  it "Brady's game total passing yards is equal to 30" do
+    expect(Stat.new.total_pass_yards(brady.first_name, brady.last_name, game.gamecode)).to eq(30)
   end
 
-  it "Brady's total passing completions is equal to 2" do
-    expect(brady.completions).to eq(2)
+  it "Brady's game total passing completions is equal to 2" do
+    expect(Stat.new.completions(brady.first_name, brady.last_name, game.gamecode)).to eq(2)
   end
 
-  it "Brady's total passing attempts is equal to 3" do
-    expect(brady.attempts).to eq(3)
+  it "Brady's game total passing attempts is equal to 3" do
+    expect(Stat.new.attempts(brady.first_name, brady.last_name, game.gamecode)).to eq(3)
   end
 
-  it "Brady's total rushing tds is equal to 1" do
-    expect(brady.total_rush_tds).to eq(1)
+  it "Brady's game total rushing tds is equal to 1" do
+    expect(Stat.new.total_rush_tds(brady.first_name, brady.last_name, game.gamecode)).to eq(1)
   end
 
-  it "Brady's total rushing yards is equal to 7" do
-    expect(brady.total_rush_yards).to eq(7)
+  it "Brady's game total rushing yards is equal to 7" do
+    expect(Stat.new.total_rush_yards(brady.first_name, brady.last_name, game.gamecode)).to eq(7)
   end
 
-  it "Brady's total rushing attempts is equal to 3" do
-    expect(brady.rushing_attempts).to eq(3)
+  it "Brady's game total rushing attempts is equal to 3" do
+    expect(Stat.new.rushing_attempts(brady.first_name, brady.last_name, game.gamecode)).to eq(3)
   end
 
-  it "Brady's total receiving tds is equal to 0" do
-    expect(brady.total_rec_tds).to eq(0)
+  it "Brady's game total receiving tds is equal to 0" do
+    expect(Stat.new.total_rec_tds(brady.first_name, brady.last_name, game.gamecode)).to eq(0)
   end
 
-  it "Brady's total receiving yards is equal to 25" do
-    expect(brady.total_rec_yards).to eq(25)
+  it "Brady's game total receiving yards is equal to 25" do
+    expect(Stat.new.total_rec_yards(brady.first_name, brady.last_name, game.gamecode)).to eq(25)
   end
 
-  it "Brady's total receptions is equal to 1" do
-    expect(brady.receptions).to eq(1)
+  it "Brady's game total receptions is equal to 1" do
+    expect(Stat.new.receptions(brady.first_name, brady.last_name, game.gamecode)).to eq(1)
   end
 
-  it "Brady's total tds is equal to 3" do
-    expect(brady.total_tds).to eq(3)
+  it "Brady's game total tds is equal to 3" do
+    expect(Stat.new.total_tds(brady.first_name, brady.last_name, game.gamecode)).to eq(3)
   end
 
-  it "Brady's total yards is equal to 62" do
-    expect(brady.total_yards).to eq(62)
+  it "Brady's game total yards is equal to 62" do
+    expect(Stat.new.total_yards(brady.first_name, brady.last_name, game.gamecode)).to eq(62)
   end
 end
