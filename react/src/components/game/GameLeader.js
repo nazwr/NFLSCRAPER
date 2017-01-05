15cdeba3b8ab30e9df57916632b31eb9b9cd8142
Nathan W <nate01776@gmail.com>
2017-01-05 13:12:34 -0500

    wrapping up styling of leader tables

--- react/src/components/game/GameLeader.js
+++ react/src/components/game/GameLeader.js
@@ -9,18 +9,20 @@ const GameLeader = props => {
     leadingPassers=
       <div>
         <div className="leader-div">
+          <p>{props.awayPassingLeader.team}</p>
            <img src= {props.awayPassingLeader.image} className="leader-image"/>
            <p>
-              {props.awayPassingLeader.team}<br/>
               <b>{props.awayPassingLeader.name}</b><br/>
               {props.awayPassingLeader.completions}/{props.awayPassingLeader.attempts}, {props.awayPassingLeader.passing_yards} yds, {props.awayPassingLeader.passing_tds} TD, {props.awayPassingLeader.interceptions} INT
             </p>
         </div>
         <div className="leader-div">
-           <img src= {props.homePassingLeader.image} className="leader-image"/>
-           <p>{props.homePassingLeader.team}</p>
-           <p>{props.homePassingLeader.name}</p>
-           <p>{props.homePassingLeader.completions}/{props.homePassingLeader.attempts}, {props.homePassingLeader.passing_yards} yds, {props.homePassingLeader.passing_tds} TD, {props.homePassingLeader.interceptions} INT</p>
+          <p>{props.homePassingLeader.team}</p>
+          <img src= {props.homePassingLeader.image} className="leader-image"/>
+          <p>
+             <b>{props.homePassingLeader.name}</b><br/>
+             {props.homePassingLeader.completions}/{props.homePassingLeader.attempts}, {props.homePassingLeader.passing_yards} yds, {props.homePassingLeader.passing_tds} TD, {props.homePassingLeader.interceptions} INT
+           </p>
         </div>
       </div>
   }
@@ -29,16 +31,20 @@ const GameLeader = props => {
     leadingRushers=
       <div>
         <div className="leader-div">
-           <img src= {props.awayRushingLeader.image} className="leader-image"/>
-           <p>{props.awayRushingLeader.team}</p>
-           <p>{props.awayRushingLeader.name}</p>
-           <p>{props.awayRushingLeader.rushing_attempts} CAR, {props.awayRushingLeader.rushing_yards} yds, {props.awayRushingLeader.rushing_tds} TD </p>
+          <p>{props.awayRushingLeader.team}</p>
+          <img src= {props.awayRushingLeader.image} className="leader-image"/>
+          <p>
+            <b>{props.awayRushingLeader.name}</b><br/>
+            {props.awayRushingLeader.rushing_attempts} CAR, {props.awayRushingLeader.rushing_yards} yds, {props.awayRushingLeader.rushing_tds} TD
+          </p>
         </div>
         <div className="leader-div">
-           <img src= {props.homeRushingLeader.image} className="leader-image"/>
-           <p>{props.homeRushingLeader.team}</p>
-           <p>{props.homeRushingLeader.name}</p>
-           <p>{props.homeRushingLeader.rushing_attempts} CAR, {props.homeRushingLeader.rushing_yards} yds, {props.homeRushingLeader.rushing_tds} TD </p>
+          <p>{props.homeRushingLeader.team}</p>
+          <img src= {props.homeRushingLeader.image} className="leader-image"/>
+          <p>
+            <b>{props.homeRushingLeader.name}</b><br/>
+            {props.homeRushingLeader.rushing_attempts} CAR, {props.homeRushingLeader.rushing_yards} yds, {props.homeRushingLeader.rushing_tds} TD
+          </p>
         </div>
       </div>
   }
@@ -47,16 +53,20 @@ const GameLeader = props => {
     leadingReceivers=
       <div>
         <div className="leader-div">
-           <img src= {props.awayReceivingLeader.image} className="leader-image"/>
-           <p>{props.awayReceivingLeader.team}</p>
-           <p>{props.awayReceivingLeader.name}</p>
-           <p>{props.awayReceivingLeader.receptions} REC, {props.awayReceivingLeader.receiving_yards} yds, {props.awayReceivingLeader.receiving_tds} TD </p>
+          <p>{props.awayReceivingLeader.team}</p>
+          <img src= {props.awayReceivingLeader.image} className="leader-image"/>
+          <p>
+            <b>{props.awayReceivingLeader.name}</b><br />
+            {props.awayReceivingLeader.receptions} REC, {props.awayReceivingLeader.receiving_yards} yds, {props.awayReceivingLeader.receiving_tds} TD
+          </p>
         </div>
         <div className="leader-div">
-           <img src= {props.homeReceivingLeader.image} className="leader-image"/>
-           <p>{props.homeReceivingLeader.team}</p>
-           <p>{props.homeReceivingLeader.name}</p>
-           <p>{props.homeReceivingLeader.receptions} REC, {props.homeReceivingLeader.receiving_yards} yds, {props.homeReceivingLeader.receiving_tds} TD </p>
+          <p>{props.homeReceivingLeader.team}</p>
+          <img src= {props.homeReceivingLeader.image} className="leader-image"/>
+          <p>
+            <b>{props.homeReceivingLeader.name}</b><br />
+            {props.homeReceivingLeader.receptions} REC, {props.homeReceivingLeader.receiving_yards} yds, {props.homeReceivingLeader.receiving_tds} TD
+          </p>
         </div>
       </div>
   }
@@ -68,14 +78,17 @@ const GameLeader = props => {
         <tr>
           <td>
             <b>Passing</b>
+            <hr />
             {leadingPassers}
           </td>
           <td>
             <b>Rushing</b>
+            <hr />
             {leadingRushers}
           </td>
           <td>
             <b>Receiving</b>
+            <hr />
             {leadingReceivers}
           </td>
         </tr>
